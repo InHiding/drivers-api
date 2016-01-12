b8e74ed8bb6ce8daee022743b578a2068dc594a8
Giuliano Melo <gcrm@ig.com.br>
2016-01-12 08:17:25 -0200

    tunning app

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -31,5 +28,11 @@ module DriversApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.template_engine false
+    end
   end
 end
