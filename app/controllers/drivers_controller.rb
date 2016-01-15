f707c7050ec74c42e417956df447446f7b2d1197
Giuliano Melo <gcrm@ig.com.br>
2016-01-15 10:20:25 -0200

    accept filters on index action

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,12 +2,28 @@ class DriversController < ApplicationController
   def index
     drivers = Driver.all
 
+    if filters = driver_filters
+      drivers = Driver.where(filters)
+    end
+
     render json: drivers
   end
 
   def show
+    driver = Driver.find(params[:id])
+    render json: driver
   end
 
   def create
+  end
+
+  private
+
+  def driver_params
+    params.require(:driver).permit(:name, :car_plate)
+  end
+
+  def driver_filters
+    params.permit(:name, :car_plate)
   end
 end
