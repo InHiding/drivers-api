7587762d7574a25acae35acca1866454725c65ad
Giuliano Melo <gcrm@ig.com.br>
2016-01-17 10:50:56 -0200

    refactor integration tests

--- test/integration/listing_drivers_test.rb
+++ test/integration/listing_drivers_test.rb
@@ -11,27 +11,19 @@ class ListingDriversTest < ActionDispatch::IntegrationTest
   test 'list drivers by name' do
     get '/drivers?name=Driver1'
 
-    assert response.success?
-
-    drivers = JSON.parse(response.body)
-    assert_equal 1, drivers.length
-    assert_equal drivers(:one).attributes, drivers.first
+    assert_found(:one)
   end 
 
   test 'list drivers by plate' do
     get '/drivers?car_plate=ABC1234'
 
-    drivers = JSON.parse(response.body)
-    assert_equal 1, drivers.length
-    assert_equal drivers(:one).attributes, drivers.first
+    assert_found(:one)
   end
 
   test 'list drivers by name and plate' do
     get '/drivers?name=Driver2&car_plate=DEF5678'
 
-    drivers = JSON.parse(response.body)
-    assert_equal 1, drivers.length
-    assert_equal drivers(:two).attributes, drivers.first
+    assert_found(:two)
   end
 
   test 'list non existant driver' do
@@ -39,5 +31,20 @@ class ListingDriversTest < ActionDispatch::IntegrationTest
 
     assert response.success?
     assert_equal [], JSON.parse(response.body)
+
+    get '/drivers?car_plate=ABC0000'
+
+    assert response.success?
+    assert_equal [], JSON.parse(response.body)
+  end
+
+  private
+
+  def assert_found(driver)
+    assert response.success?
+
+    drivers = JSON.parse(response.body)
+    assert_equal 1, drivers.length
+    assert_equal drivers(driver).attributes, drivers.first
   end
 end
