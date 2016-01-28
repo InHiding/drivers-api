0c631ab71f28c783ecf940e084e19a9d37ee6d29
Giuliano Melo <gcrm@ig.com.br>
2016-01-28 16:24:50 -0200

    driver model with tests

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,14 @@
 class Driver < ActiveRecord::Base
+  before_validation :normalize_plate
+
+  validates :name, presence: true
+  validates :car_plate, format: { with: %r{[a-zA-Z]{3}\ \d{4}\z}, 
+                                  message: I18n.t('plate_format') }
+
+  protected
+
+  def normalize_plate
+    return if car_plate.blank?
+    car_plate.upcase!
+  end
 end
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class DriverTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid empty driver' do
+    driver = Driver.new
+    assert !driver.valid?
+  end
+
+  test 'invalid without name' do
+    driver = drivers(:one)
+    driver.name = nil
+    
+    assert !driver.valid?
+    assert_match %r{can't be blank}, driver.errors[:name].join
+  end
+
+  test 'invalid without plate' do
+    driver = drivers(:one)
+    driver.car_plate = nil
+
+    assert !driver.valid?
+    assert_equal I18n.t('plate_format'), driver.errors[:car_plate].join
+  end
+
+  test 'plate with invalid format' do
+    driver = drivers(:two)
+    driver.car_plate = 'abc-1234'
+
+    assert !driver.valid?
+    assert_equal I18n.t('plate_format'), driver.errors[:car_plate].join
+  end
+
+  test 'upcase car plate' do
+    driver = drivers(:two)
+    plate = driver.car_plate.downcase!
+
+    assert driver.valid?
+    assert_equal plate.upcase, driver.car_plate 
+  end
+
+  test 'valid with all attributes' do
+    driver = drivers(:one)
+
+    assert driver.valid?
+  end
 end
