e7ca2ab23734999b77e43dddd88bf4e2ba584419
Giuliano Melo <gcrm@ig.com.br>
2016-02-01 20:43:56 -0200

    renaming nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :drivers, only: [:index, :show, :create], format: :json do
-    resources :driver_positions, path: 'status', only: ['index', 'create']
+    resources :driver_positions, path: 'status', only: ['index', 'create'], as: :status
     get 'in-area', on: :collection, to: 'driver_positions#search'
   end
 end
