017e7b744b59ad63f21e6778eb730d488e2f577d
Giuliano Melo <gcrm@ig.com.br>
2016-02-01 17:21:37 -0200

    add driver position routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :drivers, only: [:index, :show, :create], format: :json
+  resources :drivers, only: [:index, :show, :create], format: :json do
+    resources :driver_positions, path: 'status', only: ['index', 'create']
+    get 'in-area', on: :collection, to: 'driver_positions#search'
+  end
 end
